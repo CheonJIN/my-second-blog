84a389ce4acf6c317e23deb4397de5e4392d37fa
jhcheon <revival890213@gmail.com>
2019-08-13 17:52:15 +0900

    Added the draft list page in site

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,9 @@ from .forms import PostForm
 
 # Create your views here.
 def post_list(request):
-    posts = Post.objects.filter(title__contains='Test')
+    #posts = Post.objects.filter(title__contains='Test')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    #posts = Post.objects.all()
     return render(request, 'blog/post_list.html', {'posts': posts})
     
     
@@ -21,7 +23,7 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.publiched_date = timezone.now()
+            #post.publiched_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
@@ -37,10 +39,15 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
+            #post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
         
     return render(request, 'blog/post_edit.html', {'form': form})
+    
+
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
